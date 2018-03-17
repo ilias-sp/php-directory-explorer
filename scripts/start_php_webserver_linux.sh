b8f3f215f383107861a6b738acef6889d5ed1b65
ilias-sp <spiliotisilias@gmail.com>
2018-03-18 01:48:16 +0200

    added scripts to start webserver

---
+++ scripts/start_php_webserver_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# php-directory-explorer
+# https://github.com/ilias-sp/php-directory-explorer
+
+
+# to use this file, copy it to the root directory you want to explore, where the index.php resides
+
+
+php -S 0.0.0.0:33333 -t .
+
+
---
+++ scripts/start_php_webserver_windows.bat
@@ -0,0 +1,8 @@
+rem php-directory-explorer
+rem https://github.com/ilias-sp/php-directory-explorer
+
+
+rem to use this file, copy it to the root directory you want to explore, where the index.php resides
+
+php.exe -S 0.0.0.0:33333 -t .
+
